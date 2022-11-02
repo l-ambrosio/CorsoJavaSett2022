82e575a32815a21a27601ceb655b84a3efec5ed4
Ludovica <l.ambrosio@neotech.srl>
2022-11-02 13:41:45 +0100

    quinta commit

--- FabbricaDiBibite/src/srl/neotech/srl/test/Startup.java
+++ FabbricaDiBibite/src/srl/neotech/srl/test/Startup.java
@@ -1,6 +1,7 @@
 package srl.neotech.srl.test;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Startup {
 
@@ -9,7 +10,11 @@ public class Startup {
 
 	ArrayList <Cisterna> cisterna=new ArrayList<Cisterna>();
 	
-		
+	for(int i=0;i<100;i++) {
+		Random rnd=new Random();
+		int numCisterna=rnd.nextInt(100-1)+1;
+		System.out.println(numCisterna);
+	}	
 		
 	}
 
